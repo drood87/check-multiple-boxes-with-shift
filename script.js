adb039da17bab6edb3945658dd8b4b5c26273a4d
drood <dbernstein1987@hotmail.com>
2018-11-22 20:31:57 +1100

    added eventlistener

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const input = Array.from(document.querySelectorAll("input"));
-// input[1].checked = true;
-
-// if (input[2].checked === false) {
-//     alert("wassup");
-// }
 
 function myFunction(e) {
-    if (e.shiftKey) {
-        console.log(e.shiftKey);
-    }
-    if (this.checked) {
-        const checkedInputs = input.filter(inputs => inputs.checked);
-        console.log(checkedInputs);
+    const checkedInputs = input.indexOf(this);
+    console.log(checkedInputs);
+
+    if (e.checked) {
+        console.log(e);
     }
 }
 
