afd5cf7606238f5799e3b8116e66b326d1237ff8
drood <dbernstein1987@hotmail.com>
2018-11-22 19:06:17 +1100

    trying to get functions to work as intendet D:

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const input = Array.from(document.querySelectorAll("input"));
+// input[1].checked = true;
 
-console.log(input);
+// if (input[2].checked === false) {
+//     alert("wassup");
+// }
+
+function myFunction(e) {
+    if (e.shiftKey) {
+        console.log(e.shiftKey);
+    }
+    if (this.checked) {
+        const checkedInputs = input.filter(inputs => inputs.checked);
+        console.log(checkedInputs);
+    }
+}
+
+input.forEach(thatInput => thatInput.addEventListener("click", myFunction));
